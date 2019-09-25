b6dabf6e5b8b7fe805f12e303f46ebf3a535f445
McKay Waters <mckaypaswaters@gmail.com>
2019-09-25 15:58:27 -0600

    finished step 4

---
+++ src/ducks/budgetReducer.js
@@ -0,0 +1,17 @@
+// INITIAL STATE
+const initialState = {
+    purchases: [],
+    budgetLimit: null,
+    loading: false
+}
+
+// ACTION CONSTS
+
+// ACTION BUILDERS
+
+// REDUCER
+export default function reducer(state = initialState, action){
+    switch(action.type){
+        default: return state
+    }
+}
---
+++ src/ducks/userReducer.js
@@ -0,0 +1,16 @@
+// INITIAL STATE
+const initialState = {
+    email: null,
+    firstName: null,
+    lastName: null
+}
+// ACTION CONSTS
+
+// ACTION BUILDERS
+
+// REDUCER
+export default function reducer(state = initialState, action){
+    switch(action.type){
+        default: return state
+    }
+}
